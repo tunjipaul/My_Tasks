3ffd43bbf2781d8c1e7d78d04e3be38cf80b250c
TunjiPaul <ogorpaul877@gmail.com>
2025-08-15 15:11:32 +0100

    synced

--- week_2/Data_Structures/task6.py
+++ week_2/Data_Structures/task6.py
@@ -17,7 +17,28 @@
 
 #task3 simuate a football match ticket system
 #store all seat numbers using a variable.
-seat_numbers = {x for x in range(1,51)}
-booking_number = int(input("Book a seat number from 1- 50: "))
+# seat_numbers = {x for x in range(1,51)}
+# for seat in range(1,51):
+#     user_seat = booking_number = int(input("Book a seat number from (1-50): "))     
+#     seat_numbers.discard(user_seat)
+#     print("Remaining Seats:" , seat_numbers)
+
+#task4, unique voters registration.
+voters_name = set()
+voters_name = input("What is your name: ").split()
+print(voters_name)
+input(if voters_name in voters_name:
+    )print("warning")
+
+print(voters_name)
+# if voters_name in set_voters_name: 
+#     print("Warning: You have already voted!!")
+# else:
+#     print("Thank you for voting")
+
+# print(set_voters_name)
+
+
+
 
 
