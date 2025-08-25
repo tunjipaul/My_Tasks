40115c565edec789a0e029c5a2e45c81e1e3fae6
your name <ogorpaul877@gmail.com>
2025-08-25 21:52:45 +0100

    count updated

--- week_2/Data_Structures/Task6/task6.2.py
+++ week_2/Data_Structures/Task6/task6.2.py
@@ -1,9 +1,18 @@
 # task 2 unique name collector
 # a program that collects names of people attending a seminar(no duplicates) allowed and displays them in an alphabetical order.
 # use input to collect people's names 
-name = input("what is your name: ").split()
+
+# while True:
+#      name = input("What is your name: ").split()
+#      if len(name) == 3:
+#         break
+
+
 #convert the list to set.
-name_5 = set(name)
-#make the set alphabetical with "sorted" function
-name_56 = sorted(name_5)
-print(name_56)
+# name_5 = set(range(i))
+# #make the set alphabetical with "sorted" function
+# name_56 = sorted(name_5, reverse=True)
+# print(name_56)
+name = input("What is your name: ")
+for fruit in name:
+    print(f"I like {fruit}")
